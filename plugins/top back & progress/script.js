b27bd5b1ae4f49332935fa1cdd07ca83ff0e3cd5
Ali Jazbi <ali.jazbi@yahoo.com>
2022-12-26 01:02:58 +0330

    md menu updated

--- plugins/top back & progress/script.js
+++ plugins/top back & progress/script.js
@@ -1,12 +1,23 @@
 //Get the button
 let mybutton = document.getElementById("btn-back-to-top");
-
+let nav=document.getElementById("navbar");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
+  if (document.body.scrollTop > 380 ||
+    document.documentElement.scrollTop > 380) 
+    {
+    nav.style.position="fixed";
+    nav.style.boxShadow="0 4px 30px rgba(0, 0, 0, 0.1)"
+    nav.style.top = "0px";
+  } else {
+    nav.style.position="static";
+    nav.style.boxShadow="0 0 0 rgba(0, 0, 0, 0.1)"
+    nav.style.top = "-50px";
+  }
   let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let scrolled = (winScroll / height) * 100;
