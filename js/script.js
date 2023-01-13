d8cfb73a87ca5d50256e0061240dac7c67de7c97
Ali Jazbi <ali.jazbi@yahoo.com>
2023-01-13 16:04:55 +0330

    add trial contact us

--- js/script.js
+++ js/script.js
@@ -11,11 +11,20 @@
 
 
 let pop=document.getElementById("popup1");
+let pop2=document.getElementById("popup2");
           function show1() {
             pop.style.display="flex";
             document.body.style.overflow="hidden";
            }
          function hide1() {
           pop.style.display="none";
+          document.body.style.overflow="auto";
+         }
+         function show2() {
+            pop2.style.display="flex";
+            document.body.style.overflow="hidden";
+           }
+         function hide2() {
+          pop2.style.display="none";
           document.body.style.overflow="auto";
          }
