6c9f19b154b68932fbed8ccad3d7395c3186177e
Ali Jazbi <ali.jazbi@yahoo.com>
2023-01-14 18:08:44 +0330

    add single product page

--- js/script.js
+++ js/script.js
@@ -14,17 +14,17 @@ let pop=document.getElementById("popup1");
 let pop2=document.getElementById("popup2");
           function show1() {
             pop.style.display="flex";
-            document.body.style.overflow="hidden";
+            // document.body.style.overflow="hidden";
            }
          function hide1() {
           pop.style.display="none";
-          document.body.style.overflow="auto";
+          // document.body.style.overflow="auto";
          }
          function show2() {
             pop2.style.display="flex";
-            document.body.style.overflow="hidden";
+            // document.body.style.overflow="hidden";
            }
          function hide2() {
           pop2.style.display="none";
-          document.body.style.overflow="auto";
+          // document.body.style.overflow="auto";
          }
